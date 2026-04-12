355e159949cfb5f62e1a3ac2aa83b01bf34133be
murilo <murilocfcorreia@gmail.com>
2026-04-11 23:00:03 -0300

    added data structures

--- main.c
+++ main.c
@@ -1,6 +1,89 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define MAX_TAMFILA 100
 
-int main(void) {
-    printf("Hello, World!\n");
-    return 0;
+
+// estruturando as estruturas ( kkkkkkkkk )
+
+/**
+ * Pilha
+ */
+
+typedef struct
+{
+    struct elemPilha *baixo;
+    // conteudo ( transações )
+}elemPilha;
+
+typedef struct
+{
+    elemPilha *topo;
+    int nlem;
+} Pilha;
+
+
+
+/**
+*   Fila
+*/
+typedef struct
+{
+    int vetFila[MAX_TAMFILA];
+    int inicio;
+    int nlem;
+} Fila;
+
+/**
+*   Vetor Esparso
+*/
+typedef struct
+{
+    int nCol;
+    struct elemVetEsparso *coluna;
+
+} VetEsparso;
+
+typedef struct
+{
+    int col;
+    struct elemVetEsparso* direita;
+    struct elemVeTEsparso* esquerda;
+    // conteudo
+} elemVetEsparso;
+
+
+// criando métodos para as estruturas
+void criarPilha(Pilha *pilha)
+{
+    pilha->topo = NULL;
+    pilha->nlem = 0;
 }
+
+void addElementoPilha (Pilha *pilha, elemPilha *elem)
+{
+    if (pilha->topo == NULL)
+    {
+        pilha->topo = elem;
+    }
+    else
+    {
+        pilha->topo->prox = elem;
+        pilha->topo = elem;   
+    }
+}
+
+elemPilha removeElementoPilha (Pilha *pilha, elemPilha *elemRemover)
+{
+    while (pilha->topo != NULL)
+    {
+        if (  )
+    }
+}
+
+
+
+int main(void)
+{
+    printf("teste");
+}
+
