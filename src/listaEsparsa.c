18e8cbe9d01c09af331a7eadf4f52e93611ef341
murilofzcorreia <murilocfcorreia@gmail.com>
2026-04-13 17:22:04 -0300

    a b

--- src/listaEsparsa.c
+++ src/listaEsparsa.c
@@ -3,3 +3,23 @@
 //
 
 #include "listaEsparsa.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct {
+    int ID;
+    int frequencia;
+    struct statisticsPalavra *prox;
+
+}statisticsPalavra;
+
+typedef struct {
+    statisticsPalavra *listaPalavras;
+    int nPalavras;
+
+}listaEsparsaPalavrasArq;
+
+void addElemEsparso (listaEsparsaPalavrasArq *listaArq, ) {
+
+}
